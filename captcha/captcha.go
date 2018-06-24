ffcec563b6b2e55a68ff04fdf93f99d87b1c25fa
Siddharth Shukla <siddharth.shukla@uber.com>
2018-06-25 01:29:57 +0200

    Add options for configuring the captcha
    Add options for:
     - length of phrase
     - prompt message
     - error message

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -4,21 +4,55 @@ import (
 	"io"
 )
 
+const (
+	defaultCaptchaLength = 6
+)
+
 type Captcha interface {
 	ConfirmPhrase(reader io.Reader) error
 }
 
-func New() Captcha {
-	c := captchImpl{}
+func New(opts ...func(*captchaImpl)) Captcha {
+	c := captchaImpl{}
+	for _, option := range opts {
+		option(&c)
+	}
+	if c.phrase == "" {
+		c.phrase = randomString(defaultCaptchaLength)
+	}
 	return &c
 }
 
-type captchImpl struct {
+type captchaImpl struct {
+	// phrase stores the randomly generated string
 	phrase string
+	// promptMsg stores the message showing prompt asking for confirmation
 	promptMsg string
+	// errorMsg stores the message showing error in case of failure to confirm phrase
 	errorMsg string
 }
 
-func (captcha *captchImpl) ConfirmPhrase(reader io.Reader) error {
+// Length sets the length of the captcha phrase
+func Length(length int) func (captcha *captchaImpl) {
+	return func(captcha *captchaImpl) {
+		captcha.phrase = randomString(length)
+	}
+}
+
+// PromptMessage sets the message showing prompt asking for confirmation
+func PromptMessage(msg string) func (captcha *captchaImpl) {
+	return func(captcha *captchaImpl) {
+		captcha.promptMsg = msg
+	}
+}
+
+// ErrorMessage sets the message showing error in case of failure to confirm phrase
+func ErrorMessage(msg string) func (captcha *captchaImpl) {
+	return func(captcha *captchaImpl) {
+		captcha.errorMsg = msg
+	}
+}
+
+func (captcha *captchaImpl) ConfirmPhrase(reader io.Reader) error {
 	return nil
 }
--- captcha/helpers_test.go
+++ captcha/helpers_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_helpers_randomString(t *testing.T) {
+func Test_randomString_has_correct_length(t *testing.T) {
 	length := 10
 	randomStr := randomString(length)
 	require.Len(t, randomStr, length)
