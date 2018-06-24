0ef6aa4d69224ea8812e9f3ff5ebf0c1740fc7cd
Siddharth Shukla <siddharth.shukla@uber.com>
2018-06-24 19:57:07 +0200

    Add basic API for the captcha package
    Add:
      - `New` function for Captcha constructor
      - `ConfirmPhrase` method for asking confirmation

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -1,1 +1,24 @@
 package captcha
+
+import (
+	"io"
+)
+
+type Captcha interface {
+	ConfirmPhrase(reader io.Reader) error
+}
+
+func New() Captcha {
+	c := captchImpl{}
+	return &c
+}
+
+type captchImpl struct {
+	phrase string
+	promptMsg string
+	errorMsg string
+}
+
+func (captcha *captchImpl) ConfirmPhrase(reader io.Reader) error {
+	return nil
+}
