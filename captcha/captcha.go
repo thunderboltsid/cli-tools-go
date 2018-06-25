fc0ea9dc4ebd15528886744c793b865bed5d5522
Siddharth Shukla <siddharth.shukla@uber.com>
2018-06-25 02:30:18 +0200

    Add an option for Print function
    `PrintFunc` defines the formatter print function on the captcha

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -2,11 +2,17 @@ package captcha
 
 import (
 	"io"
+	"bufio"
+	"strings"
+	"github.com/sirupsen/logrus"
+	"fmt"
 )
 
 const (
 	defaultCaptchaLength = 6
 )
+
+var defaultPrintFunc = logrus.Infof
 
 type Captcha interface {
 	ConfirmPhrase(reader io.Reader) error
@@ -20,6 +26,9 @@ func New(opts ...func(*captchaImpl)) Captcha {
 	if c.phrase == "" {
 		c.phrase = randomString(defaultCaptchaLength)
 	}
+	if c.print == nil {
+		c.print = defaultPrintFunc
+	}
 	return &c
 }
 
@@ -30,29 +39,49 @@ type captchaImpl struct {
 	promptMsg string
 	// errorMsg stores the message showing error in case of failure to confirm phrase
 	errorMsg string
+	// print is the function used for formatted printing
+	print func(format string, a ...interface{})
 }
 
 // Length sets the length of the captcha phrase
-func Length(length int) func (captcha *captchaImpl) {
+func Length(length int) func(captcha *captchaImpl) {
 	return func(captcha *captchaImpl) {
 		captcha.phrase = randomString(length)
 	}
 }
 
 // PromptMessage sets the message showing prompt asking for confirmation
-func PromptMessage(msg string) func (captcha *captchaImpl) {
+func PromptMessage(msg string) func(captcha *captchaImpl) {
 	return func(captcha *captchaImpl) {
 		captcha.promptMsg = msg
 	}
 }
 
 // ErrorMessage sets the message showing error in case of failure to confirm phrase
-func ErrorMessage(msg string) func (captcha *captchaImpl) {
+func ErrorMessage(msg string) func(captcha *captchaImpl) {
 	return func(captcha *captchaImpl) {
 		captcha.errorMsg = msg
 	}
 }
 
+// PrintFunc sets the print function on the captcha
+func PrintFunc(print func(string, ...interface{})) func(captcha *captchaImpl) {
+	return func(captcha *captchaImpl) {
+		captcha.print = print
+	}
+}
+
+// ConfirmPhrase prints the prompt message and takes input from reader
 func (captcha *captchaImpl) ConfirmPhrase(reader io.Reader) error {
+	captcha.print(captcha.promptMsg)
+	bufferedReader := bufio.NewReader(reader)
+	response, err := bufferedReader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	response = strings.TrimSpace(response)
+	if response != captcha.phrase {
+		return fmt.Errorf(captcha.errorMsg)
+	}
 	return nil
 }
--- captcha/captcha_test.go
+++ captcha/captcha_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
+	"reflect"
+	"log"
 )
 
 func Test__New_creates_new_captcha(t *testing.T) {
@@ -19,6 +21,15 @@ func Test__New_creates_captcha_with_default_length(t *testing.T) {
 	assert.Len(t, ci.phrase, defaultCaptchaLength)
 }
 
+func Test__New_creates_captcha_with_default_print_function(t *testing.T) {
+	c := New()
+	require.NotNil(t, c)
+	ci, ok := c.(*captchaImpl)
+	require.True(t, ok)
+	assert.NotNil(t, ci.print)
+	assert.Equal(t, reflect.ValueOf(ci.print).Pointer(), reflect.ValueOf(defaultPrintFunc).Pointer())
+}
+
 func Test__New_creates_captcha_with_specified_length(t *testing.T) {
 	c := New(Length(10))
 	require.NotNil(t, c)
@@ -27,11 +38,20 @@ func Test__New_creates_captcha_with_specified_length(t *testing.T) {
 	assert.Len(t, ci.phrase, 10)
 }
 
-func Test__New_creates_captcha_with_prompt_and_error_msg(t *testing.T) {
+func Test__New_creates_captcha_with_specified_prompt_and_error_msg(t *testing.T) {
 	c := New(PromptMessage("prompt"), ErrorMessage("error"))
 	require.NotNil(t, c)
 	ci, ok := c.(*captchaImpl)
 	require.True(t, ok)
 	assert.Equal(t, "prompt", ci.promptMsg)
 	assert.Equal(t, "error", ci.errorMsg)
+}
+
+func Test__New_creates_captcha_with_specified_print_function(t *testing.T) {
+	c := New(PrintFunc(log.Fatalf))
+	require.NotNil(t, c)
+	ci, ok := c.(*captchaImpl)
+	require.True(t, ok)
+	assert.NotNil(t, ci.print)
+	assert.Equal(t, reflect.ValueOf(ci.print).Pointer(), reflect.ValueOf(log.Fatalf).Pointer())
 }
